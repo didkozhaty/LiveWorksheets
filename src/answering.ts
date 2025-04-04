a27b7ab9ec9b2c0ec16a2ac6bc1e75b9defc0dd0
GrishaCrysta <abobus0918@gmail.com>
2025-04-04 23:59:07 +0300

    Added support of join answers
    Now it can read and write answers joined by line

--- src/answering.ts
+++ src/answering.ts
@@ -9,6 +9,10 @@ type ButtonAnswer = {
 }
 
 const ignorableClasses = ["worksheet-blank-div", "worksheet-drop-div", "worksheet-audioplayer", "worksheet-listen-div", "worksheet-link-div", "worksheet-powerpoint-iframe", "worksheet-iframe"]
+type pos = {
+    x: number,
+    y: number
+};
 
 function isIgnorableClass(element: HTMLElement) {
     let found = false
@@ -51,7 +55,7 @@ function isButton(element: HTMLElement): boolean {
     })
     return retme
 }
-export function isSelect(element: HTMLElement) {
+function isSelect(element: HTMLElement) {
     return element.firstChild?.nodeName === 'SELECT'
 }
 function isDraggable(element: HTMLElement) {
@@ -65,11 +69,76 @@ function isDraggable(element: HTMLElement) {
     })
     return retme
 }
+function isJoin(element: HTMLElement) {
+    let retme = false;
+    const joinClasses = ["worksheet-join-div"]
+    joinClasses.forEach((value) => {
+        if(retme)
+            return
+        if(element.classList.contains(value))
+            retme = true
+    })
+    return retme
+}
+function getPos(rect: DOMRect) {
+    return {x: rect.x + rect.width / 2, y: rect.y + rect.height / 2} as pos
+}
+function getClickPos(element: HTMLElement) {
+    const rect = element.getBoundingClientRect();
+    const borderWidth = Number.parseFloat(getComputedStyle(element).borderWidth.slice(0, -2)) * 2;
+    const [height, width] = [rect.height, rect.width].map(el => (el - borderWidth) / 2)
+    const [angle, length] = [Math.random() * 2 * Math.PI, Math.random()]
+    let [x, y] = [Math.cos(angle), Math.sin(angle)]
+    const max = Math.max(Math.abs(x), Math.abs(y))
+    x /= max
+    y /= max
+    x *= length * width
+    y *= length * height
+    return {x: rect.x + rect.width / 2 + x, y: rect.y + rect.height / 2 + y} as pos
+}
+function isInRect(rect: DOMRect, position: pos) {
+    return position.x >= rect.left && position.x <= rect.right &&
+        position.y <= rect.bottom && position.y >= rect.top
+}
+let joins:[DOMRect, HTMLElement][] = []
+function answerJoin(element: HTMLElement, offset: pos) {
+    joins = [...document.querySelectorAll('.worksheet-join-div')].map(el => [el.getBoundingClientRect(), el as HTMLElement])
+    const rect = element.getBoundingClientRect()
+    const position = {x: offset.x + rect.x, y: offset.y + rect.y} as pos
+    const target = joins.find((el) => isInRect(el[0], position))
+    if(!target) {
+        console.error(`There is no join element on ${position.x};${position.y}`)
+        return
+    }
+    const thisPos = getClickPos(element)
+    const targetPos = getClickPos(target[1])
+
+    element.dispatchEvent(new MouseEvent('mousedown', {clientX: thisPos.x, clientY: thisPos.y, view: window}))
+    window.dispatchEvent(new MouseEvent('mousemove', {clientX: targetPos.x, clientY: targetPos.y, view: window}))
+    window.dispatchEvent(new MouseEvent('mouseup'))
+}
+function readJoin(element: HTMLElement) {
+    const lines = [...document.querySelectorAll('line')].map((line): [pos, pos] => {
+        const rect = line.getBoundingClientRect()
+        const [x1, x2, y1, y2] = [line.getAttribute('x1'),line.getAttribute('x2'),line.getAttribute('y1'),line.getAttribute('y2')].map(el => Number.parseFloat(el ?? ''))
+        const goesUp = (x1 > x2 && y1 > y2) || (x1 < x2 && y1 < y2)
+        return [{x: goesUp? rect.x : (rect.x + rect.width), y: rect.y} as pos, {x: goesUp? (rect.x  + rect.width) : rect.x, y: rect.y + rect.height} as pos]
+    })
+    const rect = element.getBoundingClientRect()
+    const line = lines.find((l) => isInRect(rect, l[0]) || isInRect(rect, l[1]))
+    if(!line)
+        return ''
+    const point = line.find(l => !isInRect(rect, l))
+    if(!point)
+        throw `Something went wrong ${line}`
+    const offset = {x: point.x - rect.x, y: point.y - rect.y} as pos
+    return `${offset.x};${offset.y}`;
+}
 
 export function putAnswers(answers:string):void {
     let ans = answers.split('|||')
     let i = 0
-    let doc = document.getElementById("worksheet-preview-elements")
+    const doc = document.getElementById("worksheet-preview-elements")
     const fields = doc?.children
     if (fields) {
         for (let field of fields) {
@@ -112,6 +181,10 @@ export function putAnswers(answers:string):void {
                 el.style.top = y
                 el.dispatchEvent(new MouseEvent("mouseup", {bubbles: true} ))
             }
+            else if (isJoin(el)) {
+                const dot = answer.split(';').map(el => Number.parseFloat(el))
+                answerJoin(el, {x: dot[0], y: dot[1]} as pos)
+            }
             else {
                 console.error("Unknown type", el)
             }
@@ -148,6 +221,8 @@ export function getReadyAnswers(isRaw = false) {
                 fields.push((el.firstChild as HTMLSelectElement).selectedIndex)
             } else if (isDraggable(el)) {
                 fields.push(`${el.style.left};${el.style.top}`)
+            } else if (isJoin(el)) {
+                fields.push(readJoin(el))
             }
             else {
                 fields.push('')
@@ -184,6 +259,8 @@ export function getAnswers()
             }
             else if (isDraggable(el)) {
                 fields.push(`${el.style.left};${el.style.top}`)
+            } else if (isJoin(el)) {
+                fields.push(readJoin(el))
             }
             else {
                 fields.push('')
